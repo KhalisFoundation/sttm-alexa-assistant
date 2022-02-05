33c65b9c82918e213382386cab0ffe8470edefa8
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2022-02-05 22:37:47 +0000

    updated lambda/lambda_function.py.

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -7,6 +7,8 @@
 import logging
 import ask_sdk_core.utils as ask_utils
 
+from utils import create_presigned_url
+
 from ask_sdk_core.skill_builder import SkillBuilder
 from ask_sdk_core.dispatch_components import AbstractRequestHandler
 from ask_sdk_core.dispatch_components import AbstractExceptionHandler
@@ -17,17 +19,44 @@ from ask_sdk_model import Response
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
+from ask_sdk_core.utils import is_intent_name
+from ask_sdk_core.utils import get_intent_name
+from ask_sdk_core.utils import is_request_type
+from ask_sdk_model.ui import StandardCard, Image, SimpleCard
+from ask_sdk_model.interfaces.audioplayer import (
+    PlayDirective, PlayBehavior, AudioItem, Stream, AudioItemMetadata,
+    StopDirective)
+from ask_sdk_model.interfaces import display
+
+small_image_url = create_presigned_url("Media/Note108.png")
+large_image_url = create_presigned_url("Media/Note512.png")
+
+
+audio_data = {
+    "card": {
+        "title": 'My music',
+        "text": 'I like music',
+    }
+}
+card = StandardCard(
+    title=audio_data["card"]["title"],
+    text=audio_data["card"]["text"],
+    image=Image(
+        small_image_url=small_image_url,
+        large_image_url=large_image_url
+    )
+)
 
 class LaunchRequestHandler(AbstractRequestHandler):
     """Handler for Skill Launch."""
     def can_handle(self, handler_input):
         # type: (HandlerInput) -> bool
 
-        return ask_utils.is_request_type("LaunchRequest")(handler_input)
+        return is_request_type("LaunchRequest")(handler_input)
 
     def handle(self, handler_input):
         # type: (HandlerInput) -> Response
-        speak_output = "Welcome, you can say Hello or Help. Which would you like to try?"
+        speak_output = "Hello, I can play your music, just say play my music"
 
         return (
             handler_input.response_builder
@@ -35,26 +64,7 @@ class LaunchRequestHandler(AbstractRequestHandler):
                 .ask(speak_output)
                 .response
         )
-
-
-class HelloWorldIntentHandler(AbstractRequestHandler):
-    """Handler for Hello World Intent."""
-    def can_handle(self, handler_input):
-        # type: (HandlerInput) -> bool
-        return ask_utils.is_intent_name("HelloWorldIntent")(handler_input)
-
-    def handle(self, handler_input):
-        # type: (HandlerInput) -> Response
-        speak_output = "Hello World!"
-
-        return (
-            handler_input.response_builder
-                .speak(speak_output)
-                # .ask("add a reprompt if you want to keep the session open for the user to respond")
-                .response
-        )
-
-
+    
 class HelpIntentHandler(AbstractRequestHandler):
     """Handler for Help Intent."""
     def can_handle(self, handler_input):
@@ -63,7 +73,7 @@ class HelpIntentHandler(AbstractRequestHandler):
 
     def handle(self, handler_input):
         # type: (HandlerInput) -> Response
-        speak_output = "You can say hello to me! How can I help?"
+        speak_output = "Just say play my music"
 
         return (
             handler_input.response_builder
@@ -72,37 +82,160 @@ class HelpIntentHandler(AbstractRequestHandler):
                 .response
         )
 
-
-class CancelOrStopIntentHandler(AbstractRequestHandler):
-    """Single handler for Cancel and Stop Intent."""
+class AudioPlayIntentHandler(AbstractRequestHandler):
+    # Handler for Audioplayer Play Intent
     def can_handle(self, handler_input):
         # type: (HandlerInput) -> bool
-        return (ask_utils.is_intent_name("AMAZON.CancelIntent")(handler_input) or
-                ask_utils.is_intent_name("AMAZON.StopIntent")(handler_input))
+        return is_intent_name("PlayAudio")(handler_input)
+        
+    def handle(self, handler_input):
+        # type: (HandlerInput) -> Response
+        logger.info("in AudioPlayIntent")
+        speech_text = "Welcome to my audio player"
+        audio_key = "Media/RemovalMen_Borderline.mp3"
+        
+        audio_url = create_presigned_url(audio_key)
+
+        directive = PlayDirective(
+            play_behavior=PlayBehavior.REPLACE_ALL,
+            audio_item=AudioItem(
+                stream=Stream(
+                    token=audio_key,
+                    url=audio_url,
+                    offset_in_milliseconds=0,
+                    expected_previous_token=None),
+                metadata=None))
+        handler_input.response_builder.speak(speech_text).set_card(card).add_directive(directive).set_should_end_session(True)
+
+        return handler_input.response_builder.response
+
+class AudioStopIntentHandler(AbstractRequestHandler):
+    # Handler for Stop – come here on pause or cancel too
+    def can_handle(self, handler_input):
+        # type: (HandlerInput) -> bool
+        return (is_intent_name("AMAZON.CancelIntent")(handler_input) or
+                is_intent_name("AMAZON.StopIntent")(handler_input) or
+                is_intent_name("AMAZON.PauseIntent")(handler_input))
+                
+    def handle(self, handler_input):
+        # type: (HandlerInput) -> Response
+        logger.info("in AudioStopIntent and request")
+        logger.info(handler_input.request_envelope.request)
+        speech_text = "Goodbye"
+        
+        directive = StopDirective()
+
+        handler_input.response_builder.speak(speech_text).add_directive(
+            directive).set_should_end_session(True)
+        return handler_input.response_builder.response
+
+# ########## AUDIOPLAYER INTERFACE HANDLERS #########################
+# from https://github.com/alexa/skill-sample-python-audio-player
+# This section contains handlers related to Audioplayer interface
+
+class PlaybackStartedHandler(AbstractRequestHandler):
+    """AudioPlayer.PlaybackStarted Directive received.
+    Confirming that the requested audio file began playing.
+    Do not send any specific response.
+    """
+    def can_handle(self, handler_input):
+        # type: (HandlerInput) -> bool
+        return  is_request_type("AudioPlayer.PlaybackStarted")(handler_input)
 
     def handle(self, handler_input):
         # type: (HandlerInput) -> Response
-        speak_output = "Goodbye!"
+        logger.info("In PlaybackStartedHandler")
 
-        return (
-            handler_input.response_builder
-                .speak(speak_output)
-                .response
-        )
+        return handler_input.response_builder.response
 
-class FallbackIntentHandler(AbstractRequestHandler):
-    """Single handler for Fallback Intent."""
+class PlaybackFinishedHandler(AbstractRequestHandler):
+    """AudioPlayer.PlaybackFinished Directive received.
+    Confirming that the requested audio file completed playing.
+    Do not send any specific response.
+    """
     def can_handle(self, handler_input):
         # type: (HandlerInput) -> bool
-        return ask_utils.is_intent_name("AMAZON.FallbackIntent")(handler_input)
+        return  is_request_type("AudioPlayer.PlaybackFinished")(handler_input)
 
     def handle(self, handler_input):
         # type: (HandlerInput) -> Response
-        logger.info("In FallbackIntentHandler")
-        speech = "Hmm, I'm not sure. You can say Hello or Help. What would you like to do?"
-        reprompt = "I didn't catch that. What can I help you with?"
+        logger.info("In PlaybackFinishedHandler")
+        return handler_input.response_builder.response
 
-        return handler_input.response_builder.speak(speech).ask(reprompt).response
+class PlaybackStoppedHandler(AbstractRequestHandler):
+    """AudioPlayer.PlaybackStopped Directive received.
+    Confirming that the requested audio file stopped playing.
+    Do not send any specific response.
+    """
+    def can_handle(self, handler_input):
+        # type: (HandlerInput) -> bool
+        return is_request_type("AudioPlayer.PlaybackStopped")(handler_input)
+
+    def handle(self, handler_input):
+        # type: (HandlerInput) -> Response
+        logger.info("In PlaybackStoppedHandler and request is")
+        logger.info(handler_input.request_envelope.request)
+
+        return handler_input.response_builder.response
+
+class PlaybackNearlyFinishedHandler(AbstractRequestHandler):
+    """AudioPlayer.PlaybackNearlyFinished Directive received.
+    Replacing queue with the URL again. This should not happen on live streams.
+    """
+    def can_handle(self, handler_input):
+        # type: (HandlerInput) -> bool
+        return is_request_type("AudioPlayer.PlaybackNearlyFinished")(handler_input)
+
+    def handle(self, handler_input):
+        # type: (HandlerInput) -> Response
+        logger.info("In PlaybackNearlyFinishedHandler")
+        
+        audio_key = "Media/RemovalMen_Borderline.mp3"
+        
+        audio_url = create_presigned_url(audio_key)
+
+        directive = PlayDirective(
+            play_behavior=PlayBehavior.REPLACE_ENQUEUED,
+            audio_item=AudioItem(
+                stream=Stream(
+                    token=audio_key,
+                    url=audio_url,
+                    offset_in_milliseconds=0,
+                    expected_previous_token=None),
+                metadata=None))     
+        handler_input.response_builder.set_card(card).add_directive(directive).set_should_end_session(True)
+        return handler_input.response_builder.response
+
+class PlaybackFailedHandler(AbstractRequestHandler):
+    """AudioPlayer.PlaybackFailed Directive received.
+    Logging the error and stoprestarting playing with no output speech and card.
+    """
+    def can_handle(self, handler_input):
+        # type: (HandlerInput) -> bool
+        return is_request_type("AudioPlayer.PlaybackFailed")(handler_input)
+
+    def handle(self, handler_input):
+        # type: (HandlerInput) -> Response
+        request = handler_input.request_envelope.request
+        logger.info("Playback failed: {}".format(request.error))
+
+        return handler_input.response_builder.response
+
+class ExceptionEncounteredHandler(AbstractRequestHandler):
+    """Handler to handle exceptions from responses sent by AudioPlayer
+    request.
+    """
+    def can_handle(self, handler_input):
+        # type; (HandlerInput) -> bool
+        return is_request_type("System.ExceptionEncountered")(handler_input)
+
+    def handle(self, handler_input):
+        # type: (HandlerInput) -> Response
+        logger.info("\n**************** EXCEPTION *******************")
+        logger.info(handler_input.request_envelope)
+        return handler_input.response_builder.response
+
+# ###################################################################
 
 class SessionEndedRequestHandler(AbstractRequestHandler):
     """Handler for Session End."""
@@ -112,11 +245,11 @@ class SessionEndedRequestHandler(AbstractRequestHandler):
 
     def handle(self, handler_input):
         # type: (HandlerInput) -> Response
+        logger.info("in SessionEnded")
 
         # Any cleanup logic goes here.
 
         return handler_input.response_builder.response
-
 
 class IntentReflectorHandler(AbstractRequestHandler):
     """The intent reflector is used for interaction model testing and debugging.
@@ -126,7 +259,7 @@ class IntentReflectorHandler(AbstractRequestHandler):
     """
     def can_handle(self, handler_input):
         # type: (HandlerInput) -> bool
-        return ask_utils.is_request_type("IntentRequest")(handler_input)
+        return is_request_type("IntentRequest")(handler_input)
 
     def handle(self, handler_input):
         # type: (HandlerInput) -> Response
@@ -140,7 +273,6 @@ class IntentReflectorHandler(AbstractRequestHandler):
                 .response
         )
 
-
 class CatchAllExceptionHandler(AbstractExceptionHandler):
     """Generic error handling to capture any syntax or routing errors. If you receive an error
     stating the request handler chain is not found, you have not implemented a handler for
@@ -153,6 +285,11 @@ class CatchAllExceptionHandler(AbstractExceptionHandler):
     def handle(self, handler_input, exception):
         # type: (HandlerInput, Exception) -> Response
         logger.error(exception, exc_info=True)
+        logger.info("exception")
+        logger.info(exception)
+        #logger.info(ask_utils.get_intent_name(handler_input))
+        #The provided request is not an IntentRequest
+        logger.info(ask_utils.get_request_type(handler_input))
 
         speak_output = "Sorry, I had trouble doing what you asked. Please try again."
 
@@ -163,18 +300,24 @@ class CatchAllExceptionHandler(AbstractExceptionHandler):
                 .response
         )
 
+
 # The SkillBuilder object acts as the entry point for your skill, routing all request and response
 # payloads to the handlers above. Make sure any new handlers or interceptors you've
 # defined are included below. The order matters - they're processed top to bottom.
 
-
 sb = SkillBuilder()
 
 sb.add_request_handler(LaunchRequestHandler())
-sb.add_request_handler(HelloWorldIntentHandler())
 sb.add_request_handler(HelpIntentHandler())
-sb.add_request_handler(CancelOrStopIntentHandler())
-sb.add_request_handler(FallbackIntentHandler())
+sb.add_request_handler(AudioPlayIntentHandler())
+sb.add_request_handler(AudioStopIntentHandler())
+# ########## AUDIOPLAYER INTERFACE HANDLERS #########################
+sb.add_request_handler(PlaybackStartedHandler())
+sb.add_request_handler(PlaybackFinishedHandler())
+sb.add_request_handler(PlaybackStoppedHandler())
+sb.add_request_handler(PlaybackNearlyFinishedHandler())
+sb.add_request_handler(PlaybackFailedHandler())
+sb.add_request_handler(ExceptionEncounteredHandler())
 sb.add_request_handler(SessionEndedRequestHandler())
 sb.add_request_handler(IntentReflectorHandler()) # make sure IntentReflectorHandler is last so it doesn't override your custom intent handlers
 
