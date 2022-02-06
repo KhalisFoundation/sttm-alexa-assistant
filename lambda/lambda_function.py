8eb4eb7e4b610ccc00f3a2e08393830a65807617
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2022-02-06 01:05:35 +0000

    updated lambda/lambda_function.py.

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -56,7 +56,7 @@ class LaunchRequestHandler(AbstractRequestHandler):
 
     def handle(self, handler_input):
         # type: (HandlerInput) -> Response
-        speak_output = "Hello, I can play your music, just say play my music"
+        speak_output = "Waheguru ji da Khalsa, Waheguru ji di fateh. How Can I help you today"
 
         return (
             handler_input.response_builder
