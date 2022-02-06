9b7cd588817a882b5daa75fa95464d1f966084c1
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2022-02-06 01:52:57 +0000

    updated lambda/track_info.py.

---
+++ lambda/track_info.py
@@ -0,0 +1,14 @@
+track_info = [
+{
+    "genre" : "AmritBaani",
+    "title" : "Todays Hukamnama",
+    "artist" :"Sevadaar Ji",
+    "url" : "Media/Punjabi Hukam Katha-Ang 616.mp3"
+},
+{
+    "genre" : "AmritBaani",
+    "title" : "Todays Hukamnama",
+    "artist" :"Sevadaar Ji",
+    "url" : "Media/Punjabi Hukam Katha-Ang 616.mp3"
+},
+]
