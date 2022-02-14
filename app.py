b31e9f74311a5e420a2b20a24dcf9fe931de44ff
Gurmehar Singh Khalsa <gurmeharsinghkhalsa1005@gmail.com>
2022-02-14 08:09:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,31 +1,49 @@
+#!/usr/bin/env python3.7.7
 import logging
-
-from random import randint
 import banidb
-
-from flask import Flask, render_template
-
-from flask_ask import Ask, statement,  question, session
-
+import better_translit as bt
+from flask import Flask, request
 
 app = Flask(__name__)
-ask = Ask(app, "/")
 
 logging.getLogger("flask_ask").setLevel(logging.DEBUG)
 
-
-@ask.launch
+@app.route('/')
 def launch():
-
-    welcome_msg = render_template('Waheguru ji ka Khalsa, Waheguru ji ki Fateh')
-    return statement(welcome_msg)
-
-
-@ask.intent("Random_Shabad")
-def shabad_random():
-    random_shabad = banidb.random()['verses'][0]['verse']
-    msg = f"Waheguru ji, Here is Random Shabad {random_shabad}"
-    return statement(random_shabad)
+    req = request.get_json(silent=True, force=True)
+    type = req.get('type')
+    json = {
+            "version": "1.0",
+            "response": {
+                "outputSpeech": {},
+                "reprompt": {
+                    "outputSpeech": {
+                        "type": "PlainText",
+                        "text": "Can I help you with anything?"
+                    }
+                },
+                "shouldEndSession": False
+            }
+        }
+    if (type=='LaunchRequest'):
+        output = {
+            "type": "SSML",
+            "text": "Waheguru ji ka Khalsa, Waheguru ji ki Fateh?",
+            "ssml": "<speak><phoneme alphabet=\"ipa\" ph=\"ʋɑhɪGʊru d͡ʒi kɑ kʰɑlsa, ʋɑhɪGʊru d͡ʒi ki fət̪eh\">Waheguru ji ka Khalsa, Waheguru ji ki Fateh</phoneme></speak>"
+        }
+        json['response']['outputSpeech'] = output
+    elif (type=='IntentRequest'):
+        intent = req.get('intent').get('name')
+        if (intent=='Random_Shabad'):
+            random_shabad = bt.better(banidb.random()['verses'][0]['verse'])
+            msg = f"Waheguru ji, Here is Random Shabad {random_shabad}"
+            output = {
+                "type": "SSML",
+                "text": random_shabad,
+                "ssml": msg
+            }
+            json['response']['outputSpeech'] = output
+    return json
 
 
 if __name__ == '__main__':
